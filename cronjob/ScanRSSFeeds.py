0271d5df446eee28b7dfabd46b560f668fb2c64f
Chris Bednar <cbednar@bulletinnews.local>
2019-04-11 14:05:09 -0400

    Save results to database with primary key using the url... also a proper unix date is being used.

--- cronjob/ScanRSSFeeds.py
+++ cronjob/ScanRSSFeeds.py
@@ -1,16 +1,28 @@
 import feedparser
+import sqlite3
+from dateutil.parser import parse
+import time
+
+sqlite_file = 'vegan_food_news_links.sqlite'
+
 
 def saveToDatabase(entries):
+	conn = sqlite3.connect(sqlite_file)
+	c = conn.cursor()
+
 	for entry in entries:
-		title = entry["title"]
+		headline = entry["title"].encode("utf-8")
 		link = entry["links"][0]["href"]
 		published = entry["published"]
-		#summary = entry["summary"]
-		print(title.encode("utf-8"))
-		print(link)
-		print(published)
-		print("---")
-		print("---")
+		published_date = parse(published)
+		published_date_unix = time.mktime(published_date.timetuple()) 
+		
+		c.execute("INSERT OR IGNORE INTO rss_feed_item (published_date, url, headline, is_posted) VALUES (?, ?, ?, 0)", (published_date_unix, link, headline.decode('utf-8')))
+		
+	conn.commit()
+	conn.close()
+		
+		
 
 feeds = ["https://vegnews.com/feed.rss", "https://www.plantbasednews.org/post/rss.xml", "https://www.onegreenplanet.org/channel/vegan-food/feed/", "https://www.onegreenplanet.org/channel/vegan-food/news-and-trends/feed/", "https://www.livekindly.co/feed/", "http://cok.net/blog/category/veg-eating/feed/"]
 
@@ -22,3 +34,4 @@ for feed in feeds:
 	
 		
 		
+
--- newdb.py
+++ newdb.py
@@ -7,14 +7,19 @@ conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 # Creating a new SQLite table with 1 column
-#c.execute('CREATE TABLE rss_feed_item (published_date INTEGER, url TEXT, headline TEXT, is_posted INTEGER)')
-#c.execute('CREATE INDEX published_date_idx on links (published_date)')
+#c.execute('CREATE TABLE rss_feed_item (published_date INTEGER, url TEXT primary key, headline TEXT, is_posted INTEGER)')
+#c.execute('CREATE INDEX published_date_idx on rss_feed_item (published_date)')
 
 #c.execute("INSERT INTO rss_feed_item (published_date, url, headline, is_posted) VALUES (1554923716, 'https://www.unixtimestamp.com/', 'This is the headline', 0)")
 
-#c.execute('SELECT * FROM rss_feed_item where is_posted = 0')
+c.execute('SELECT headline, url, published_date FROM rss_feed_item where is_posted = 0 order by published_date desc')
 #all_rows = c.fetchall()
 #print('1):', all_rows)
+
+for row in c:
+	print(row)
+	print("---")
+	print("---")
 
 # Committing changes and closing the connection to the database file
 conn.commit()
